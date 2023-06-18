e028cc9a33663ee0a1f75b0a3b689f4feab42bf1
Petya Todorova <invisioncl@gmail.com>
2023-06-18 10:32:01 +0300

    Error: Username already exists!

--- Petstagram-Skeleton/src/managers/userManager.js
+++ Petstagram-Skeleton/src/managers/userManager.js
@@ -4,7 +4,15 @@ exports.login = (username, password) => {
 
 }
 
-exports.register = (userData) => User.create(userData);
+// exports.register = (userData) => User.create(userData);
+exports.register = async (userData) => {
+    const user = User.findOne({username: userData.username});
+    if (user) {
+        throw new Error('Username already exists!')
+    }
+
+    return User.create(userData);
+}
 
 exports.logout = () => {
 
--- Petstagram-Skeleton/src/models/User.js
+++ Petstagram-Skeleton/src/models/User.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
         minLength: 2,
     },
     email: {
